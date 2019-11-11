0c3b5c76ad154f9195b94bc5832c596c892bfc23
nicole.taylor <ntaylor@uship.com>
2019-11-11 11:59:06 -0600

    let's use filter

--- src/practice1.js
+++ src/practice1.js
@@ -2,6 +2,20 @@ const globalVars = require('./global');
 
 const pickIngredients = (isVegan) => {
     const { ingredients } = globalVars;
-    
 
+    const tacoIngredients = ingredients.filter(function (ingredient) {
+        if (isVegan === true) {
+            return (ingredient.isVegan);
+        }
+
+        return true;
+    });
+
+    return tacoIngredients;
 }
+
+console.log(`Taco Ingredients for a Vegan Taco are`);
+console.log(pickIngredients(true));
+
+console.log(`Taco Ingredients for an All out awesome Taco are`);
+console.log(pickIngredients(false));
